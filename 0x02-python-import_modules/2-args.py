c5204ea7da32da93526f7f7efabd55f6ead46ccf
Getayawkal Wondimagegnehu <Getayawkal.won@gmail.com>
2023-10-08 08:56:05 -0700

    Changing 2args

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,13 +2,9 @@
 if __name__ == "__main__":
     import sys
     args = len(sys.argv)
-    print("{} ".format(args - 1), end="")
     if args == 1:
-        print("arguments.")
-    elif args > 1:
-        if args == 2:
-            print("argument:")
-        elif args > 2:
-            print("arguments:")
-        for i in range(args - 1):
-            print("{}: {}".format((i + 1), sys.argv[i + 1]))
+        print("{} arguments.".format(args-1))
+    if args > 1:
+        for i in range(1, args):
+            print("{} arguments:".format(args-1))
+            print("{}: {}".format(i, sys.argv[i]))
