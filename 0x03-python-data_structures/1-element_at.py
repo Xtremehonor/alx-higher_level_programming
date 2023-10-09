1ea20a33db1e9400e963165e17a1f71903c83820
Getayawkal Wondimagegnehu <Getayawkal.won@gmail.com>
2023-10-09 14:17:02 -0700

    adding 1-ele

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -3,8 +3,7 @@ def element_at(my_list, idx):
     str_len = 0
     for i in my_list:
         str_len += 1
-
-    if  idx < 0 or idx > str_len:
+    if  idx < 0 or idx >= str_len:
         return None
     else:
         return my_list[idx]
