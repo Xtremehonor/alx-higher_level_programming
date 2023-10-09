b7f4b5dc6db697a1e02900e1837df8e56926e828
Getayawkal Wondimagegnehu <Getayawkal.won@gmail.com>
2023-10-09 16:48:18 -0700

    adding 9-max

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    length = len(sentence)
-    first_char = sentence[0] if length > 0 else "None"
-    tup = length, first_char
-    return(tup)
+    total = len(sentence)
+    char = sentence[0] if total > 0 else "None"
+    new = total, char
+    return(new)
