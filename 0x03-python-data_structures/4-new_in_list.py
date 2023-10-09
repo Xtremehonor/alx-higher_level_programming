bfe81546b9b25a1871e1099ebfd2a2ef42112a15
Getayawkal Wondimagegnehu <Getayawkal.won@gmail.com>
2023-10-09 16:04:23 -0700

    adding 4-new

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    list_cp = my_list.copy()
-    if idx < 0 or idx >= len(my_list):
-        return list_cp
-    elif idx in range(0, (len(my_list) - 1)):
-        my_list[idx] = element
-        return list_cp
+    dup = my_list[:]
+    if idx >= 0 and idx < len(dup):
+        dup[idx] = element
+    return dup
