91e06db7e0b899e2111033375516609f1ad23dfa
Getayawkal Wondimagegnehu <Getayawkal.won@gmail.com>
2023-10-09 13:31:01 -0700

    1-element

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
     str_len = len(my_list)
-    if idx > str_len and idx < 0:
+    if idx > str_len or idx < 0:
         return None
     else:
         return my_list[idx]
