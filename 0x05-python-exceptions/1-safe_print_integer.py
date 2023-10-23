ab2f3aeb02cce009278d62d0be73e73aa9754926
Xtremehonor <Xtremehonor@gmail.com>
2023-10-23 22:25:58 +0300

    1-safe_print_integer.py

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,16 @@
+#!usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+
+    y = 0;
+
+    while True:
+          try:
+               if y < x:
+                     print(my_list[y], end='')
+                     y += 1
+               else:
+                    print()
+                    return y
+          except IndexError:
+               print()
+               return y
