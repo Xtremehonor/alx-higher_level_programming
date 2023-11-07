cb7aa56abdb5ff09211d7b24b286dc59300ee9ee
Xtremehonor <Xtremehonor@gmail.com>
2023-11-08 01:56:37 +0300

    1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+    Writes a string to a text file (UTF8)
+    and returns the number of characters written.
+    """
+
+
 def write_file(filename="", text=""):
     """
     Writes a string to a text file (UTF8)
