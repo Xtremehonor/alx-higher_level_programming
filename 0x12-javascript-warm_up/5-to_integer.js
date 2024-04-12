f7023c01f6081acbbceb94f904d6cd63741ff412
Xtremehonor <xtremehonor@gmail.com>
2024-04-12 16:23:48 -0700

    corrected 5-to_integer

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -2,7 +2,7 @@
 const myNumber = process.argv[2];
 
 if (isNaN(myNumber)) {
-    console.log('Not a number');
+  console.log('Not a number');
 } else {
-    console.log('My number: ' + parseInt(myNumber));
+  console.log('My number: ' + parseInt(myNumber));
 }
