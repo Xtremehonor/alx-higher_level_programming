b2b2d7e4a89449d2c78bd0c816ae8c9ebd6c2a7c
Xtremehonor <xtremehonor@gmail.com>
2024-04-12 14:36:19 -0700

    added semistandard remarks

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 //  this alert if argument is found or not
 
-if (process.argv.length > 2) {
+if (process.argv[2]) {
   console.log('Argument found');
 } else {
   console.log('No argument');
