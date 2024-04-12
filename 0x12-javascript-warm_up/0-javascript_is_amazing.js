ea13bd581dbf6f7bb0e323e4320996c3dd79a43f
xtremehonor@gmail.com <xtremehonor@email.com>
2024-04-12 18:46:19 +0300

    added 1-multi_languages

--- 0x12-javascript-warm_up/0-javascript_is_amazing.js
+++ 0x12-javascript-warm_up/0-javascript_is_amazing.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
 
+//this prints 'Javascript is amazing'
+
 const myVar = 'JavaScript is amazing'
 console.log(myVar)
--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,7 @@
 #!/usr/bin/node
 
+//this prints stuff
+
 console.log('C is fun');
 console.log('Python is cool');
 console.log('JavaScript is amazing');
