130c21ebe9d2657fd8e7f0d6f7813af0e2d59985
Xtremehonor <xtremehonor@gmail.com>
2024-04-12 14:33:46 -0700

    added semistandard remarks

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 //  this prints the first argment passed to it
 
-if (process.argv[2] === undefind) {
+if (!process.argv[2]) {
   console.log('No argument');
 } else {
   console.log(process.argv[2]);
