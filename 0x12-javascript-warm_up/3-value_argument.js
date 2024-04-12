27b4d31a218204fcfd7faba6b33cae1d721b38da
Xtremehonor <xtremehonor@gmail.com>
2024-04-12 14:06:32 -0700

    added semistandard remarks

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,7 +2,7 @@
 //  this prints the first argment passed to it
 
 if (process.argv[2]) {
-    console.log(process.argv[2])
+  console.log(process.argv[2]);
 } else {
-    console.log('No argument');
+  console.log('No argument');
 }
