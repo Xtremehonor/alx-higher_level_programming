4d8bf45b3458acacd61b65142501c022cd9f2f24
Xtremehonor <xtremehonor@gmail.com>
2024-04-13 02:46:30 -0700

    Adding 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,13 +2,13 @@
 const xTime = process.argv[2];
 
 if (xTime > 0) {
+  for (let i = 0; i < xTime; i++) {
+    let output = '';
     for (let i = 0; i < xTime; i++) {
-        let output = '';
-        for (let i = 0; i < xTime; i++) {
-            output += 'x';
-        }
-        console.log(output);
+      output += 'x';
     }
+    console.log(output);
+  }
 } else {
-    console.log('Missing size');
+  console.log('Missing size');
 }
