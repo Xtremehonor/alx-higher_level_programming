e51c2942a199a4403122e45824b64319a459f2ad
xtremehonor@gmail.com <xtremehonor@email.com>
2024-04-27 17:47:47 +0300

    added 3-rectangle

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,16 +1,15 @@
 #!/usr/bin/node
 module.exports = class Rectangle {
-  constructor (w, h) {
+  constructor(w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     } else {
-      this.width = 0; // Default values for invalid input
-      this.height = 0;
+      return {}
     }
   }
 
-  print () {
+  print() {
     for (let i = 0; i < this.height; i++) {
       let row = '';
       for (let j = 0; j < this.width; j++) {
