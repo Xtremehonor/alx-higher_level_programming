f12a1b7479866a5ac7bbf1c9b4f81246a7157585
Xtremehonor <xtremehonor@gmail.com>
2024-05-01 13:00:49 -0700

    adding executable

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
 # Lists all states from the database hbtn_0e_0_usa.
-# Usage: ./0-select_states.py <mysql username> \
-#                             <mysql password> \
-#                             <database name>
+# Usage: ./0-select_states.py <user> \
+#                             <passwd> \
+#                             <db>
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost',
+    db = MySQLdb.connect(host="localhost",
                          user=sys.argv[1],
                          passwd=sys.argv[2],
-                         db=sys.argv[3], 
+                         db=sys.argv[3],
                          port=3306)
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states ORDER BY id ASC")
@@ -20,4 +20,3 @@ if __name__ == "__main__":
         print(row)
     cursor.close()
     db.close()
-
