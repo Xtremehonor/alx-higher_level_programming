7cb868556c0a723165c92f662873eaef319b643e
Xtremehonor <xtremehonor@gmail.com>
2024-05-01 13:56:05 -0700

    Adding 2-my filter

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
                          port=3306)
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'"
-                .format(sys.argv[4]))
+                   .format(sys.argv[4]))
 
     exists = cursor.fetchall()
     for row in exists:
