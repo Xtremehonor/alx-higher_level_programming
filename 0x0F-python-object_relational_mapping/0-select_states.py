655f763a2a1861bf23cedac8796d1be6a4776d1d
xtremehonor@gmail.com <xtremehonor@email.com>
2024-05-01 18:58:31 +0300

    changed somethings on 0-select_states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -6,12 +6,17 @@
 import MySQLdb
 import sys
 
+host = 'localhost'
+user=sys.argv[1]
+password=sys.argv[2]
+db =sys.argv[3]
+
+
 if __name__ == "__main__": 
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1], passwd=sys.argv[2], db =sys.argv[3])
+    db = MySQLdb.connect(host=host, user=user, passwd=password, db =db[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM `states`")
 
     exists = cursor.fetchall()
     for row in exists:
         print(row)
-        
