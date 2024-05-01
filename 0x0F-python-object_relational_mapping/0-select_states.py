f919a629cbc8de4c50d9d6cf6a1427aa2823d018
xtremehonor@gmail.com <xtremehonor@email.com>
2024-05-01 19:13:40 +0300

    changed somethings on 0-select_states

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,9 +7,9 @@ import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host='localhost', 
-                         user=sys.argv[1], 
-                         passwd=sys.argv[2], 
+    db = MySQLdb.connect(host='localhost',
+                         user=sys.argv[1],
+                         passwd=sys.argv[2],
                          db =sys.argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM `states`")
