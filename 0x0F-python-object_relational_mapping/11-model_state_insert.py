e06e8f4fc28f65a2ef995d70454ac0f0272b2b2f
Xtremehonor <xtremehonor@gmail.com>
2024-05-03 15:34:41 -0700

    11-model

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-
-
-from sqlalchemy import create_engine
+"""
+All states via SQLAlchemy
+"""
+from sys import argv
 from model_state import Base, State
-from sqlalchemy.orm import sessionmaker
-import sys
+from sqlalchemy import (create_engine)
+from sqlalchemy.orm import Session
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
                            format(argv[1], argv[2], argv[3]),
                            pool_pre_ping=True)
